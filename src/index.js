d4d2452d903c9d3bf3ee90fd5867afd70ad84660
Indrawat <yash.indrawat@getzephyr.com>
2018-03-25 00:13:13 +0530

    Added Express Server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './modules/EntryModule/EntryModuleComponent';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public/');
+var APP_DIR = path.resolve(__dirname, 'src/');
+
+var config = {
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?/,
+                include: APP_DIR,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['css-loader']
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
